fad032c48271e942867aaffda8c207f8fb87683c
daniel.damyanov <daniel.damyanov@primeholding.com>
2023-03-08 14:33:21 +0200

    .

--- src/CarData/Cars.Database/ApplicationDbContext.cs
+++ src/CarData/Cars.Database/ApplicationDbContext.cs
@@ -22,7 +22,64 @@ namespace Cars.Database
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Car>().HasData(new Car() {CarBrand = "BMW" });
+            var cars = new List<Car>() {
+                new Car() { CarBrand = "BMW", CarModel="314",CarScheduler=new CarScheduler(){VisitAt=new DateTime(2023,3,10) } } ,
+                new Car() { CarBrand = "Audi",CarModel="A6",CarScheduler=new CarScheduler(){VisitAt=new DateTime(2023,3,10) } } ,
+                new Car() { CarBrand = "Mercedes",CarModel="s220",CarScheduler=new CarScheduler(){VisitAt=new DateTime(2023,3,11) } } ,
+                new Car() { CarBrand = "Ford", CarModel="Focus",CarScheduler=new CarScheduler(){VisitAt=new DateTime(2023,3,9) } } ,
+                new Car() { CarBrand = "Peugeot",CarModel="205",CarScheduler=new CarScheduler(){VisitAt=new DateTime(2023,3,12) } } };
+            modelBuilder.Entity<Car>().HasData(cars);
+
+            var carProblems = new List<CarProblems>()
+            {
+                new CarProblems()
+                {
+                    CarId=cars[0].Id,
+                    Description="Change the clutch",
+                    CarPart="Clutch",
+                    TotalAmount=1000
+                },
+                 new CarProblems()
+                {
+                    CarId=cars[0].Id,
+                    Description="Breaks cylinders",
+                    CarPart="breaks",
+                    TotalAmount=250
+                }
+                 ,
+                 new CarProblems()
+                {
+                    CarId=cars[1].Id,
+                    Description="FIlters and oils",
+                    CarPart="Engine",
+                    TotalAmount=350
+                },
+                 
+                 new CarProblems()
+                {
+                    CarId=cars[2].Id,
+                    Description="Timing belt change",
+                    CarPart="engine",
+                    TotalAmount=1300
+                }
+                 ,
+                 new CarProblems()
+                {
+                    CarId=cars[3].Id,
+                    Description="Shocking absorbers",
+                    CarPart="wheels",
+                    TotalAmount=600
+                },
+                 new CarProblems()
+                {
+                    CarId=cars[4].Id,
+                    Description="Gear bearing",
+                    CarPart="transmission",
+                    TotalAmount=900
+                }
+            };
+            modelBuilder.Entity<CarProblems>().HasData(carProblems);
+
 
         }
     }
